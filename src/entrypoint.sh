6e342821d12de03b57bd33497b2beef9d7e166fe
Jürgen Viljaste <j.viljaste@gmail.com>
2015-02-23 19:08:05 +0200

    Update

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,9 +2,9 @@
 
 case "${1}" in
   build)
-    /bin/su - root -mc "apt-get update && /src/build.sh && /src/clean.sh"
+    /bin/su - root -mc "apt-get update && /src/base/build.sh && /src/base/clean.sh"
     ;;
   run)
-    /bin/su - root -mc "source /src/variables.sh && /src/run.sh"
+    /bin/su - root -mc "source /src/base/variables.sh && /src/base/run.sh"
     ;;
 esac
